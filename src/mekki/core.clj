ae4936bd44f2398a610b4607994d07baf9ee711e
OHTA Shogo <athos0220@gmail.com>
2015-08-14 01:12:41 +0900

    Sketch DSL

--- src/mekki/core.clj
+++ src/mekki/core.clj
@@ -1,6 +1,23 @@
 (ns mekki.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(comment
+
+  (defsig ^:abstract A [])
+  (defsig B [])
+
+  (defsig ^:one A1 [A])
+  (defsig ^:one A2 [A])
+
+  (deffields A
+    [f :- lone B -> lone B]
+    [g :- B])
+
+  (defpred some-g []
+    (some g))
+
+  (defpred at-most-3 [x :- Univ, y :- Univ]
+    (>= (count (+ x y)) 3))
+
+  (run (and (some A) (at-most-3 B B)))
+
+)
