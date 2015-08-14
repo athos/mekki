5629b5dfa50d5a9e7328198f5798adc50ec2cdee
OHTA Shogo <athos0220@gmail.com>
2015-08-14 15:23:27 +0900

    Refine DSL example

--- src/mekki/core.clj
+++ src/mekki/core.clj
@@ -8,20 +8,23 @@
 
 (comment
 
-  (defsig ^:abstract A [])
-  (defsig B [])
-
-  (defsig ^:one A1 [A])
-  (defsig ^:one A2 [A])
-
-  (deffields A
-    [f :- lone B -> lone B]
+  (defsig B)
+  (defsig ^:abstract A
+    [f :- (lone->lone B B)]
     [g :- B])
+
+  (defsig ^:one A1 :extends A)
+  (defsig ^:one A2 :extends A)
+
+  (fact
+    (all [this :- A]
+      (in (.f this) (lone->lone B B))
+      (in (.g this) B)))
 
   (defpred some-g []
     (some g))
 
-  (defpred at-most-3 [x :- Univ, y :- Univ]
+  (defpred at-most-3 [x :- univ, y :- univ]
     (>= (count (+ x y)) 3))
 
   (run (and (some A) (at-most-3 B B)))
