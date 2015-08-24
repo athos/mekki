09005656897bf493c4bd9050f1055d0363997fe1
OHTA Shogo <athos0220@gmail.com>
2015-08-24 13:51:16 +0900

    Refactor code

--- src/mekki/core.clj
+++ src/mekki/core.clj
@@ -132,15 +132,19 @@
 (defmacro expr [e]
   (compile #{} e))
 
-(defn run-fn [e & {:keys [ns sigs] :or {ns *ns*}}]
-  (let [sigs (or sigs
-                 (for [[_ v] (ns-publics ns)
-                       :when (= (:tag (meta v)) Sig)]
-                   (deref v)))
-        cmd (Command. false 3 3 3 e)
+(defn- execute [e sigs check?]
+  (let [cmd (Command. check? 3 3 3 e)
         opts (A4Options.)]
     (set! (.solver opts) (A4Options$SatSolver/SAT4J))
     (TranslateAlloyToKodkod/execute_command A4Reporter/NOP sigs cmd opts)))
+
+(defn- ns-sigs [ns]
+  (for [[_ v] (ns-publics (the-ns ns))
+        :when (= (:tag (meta v)) Sig)]
+    (deref v)))
+
+(defn run-fn [e & {:keys [ns sigs] :or {ns *ns*}}]
+  (execute e (or sigs (ns-sigs ns)) false))
 
 (defmacro run [e]
   `(run-fn (expr ~e)))
