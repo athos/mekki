32628dc4f8ab6f77887f9e22f00d5feb5ba7c19a
OHTA Shogo <athos0220@gmail.com>
2015-09-07 23:54:10 +0900

    WIP: Implement adhoc enumerator

--- src/mekki/viz.clj
+++ src/mekki/viz.clj
@@ -2,30 +2,40 @@
   (:require [mekki
              [lang :as lang]
              [exec :as exec]])
-  (:import [edu.mit.csail.sdg.alloy4compiler.translator A4Solution]
+  (:import [edu.mit.csail.sdg.alloy4compiler.translator A4Solution A4SolutionReader]
            [edu.mit.csail.sdg.alloy4viz VizGUI]
-           [edu.mit.csail.sdg.alloy4 Computer]
+           [edu.mit.csail.sdg.alloy4 Computer XMLNode]
            [java.io File]))
 
-(defn make-enumerator []
-  (reify Computer
-    (compute [this input]
-      (println "invoked compute!!"))))
+(defn- make-enumerator [sigs]
+  (let [sol (volatile! nil)]
+    (reify Computer
+      (compute [this filename]
+        (when (= @sol :done)
+          (when-not @sol
+            (let [xml (XMLNode. (File. filename))]
+              (vreset! sol (A4SolutionReader/read sigs xml))))
+          (let [^A4Solution sol' @sol]
+            (when (.satisfiable sol')
+              (.writeXML sol' filename)
+              (vreset! sol (.next sol'))
+              filename)))))))
 
-(defn show [^A4Solution sol]
-  (let [^File temp (File/createTempFile "mekki_solution" ".xml")
+(defn show [^A4Solution sol & {:keys [ns sigs] :or {ns *ns*}}]
+  (let [sigs (or sigs (exec/ns-sigs ns))
+        ^File temp (File/createTempFile "mekki_solution" ".xml")
         filename (.getPath temp)]
     (.writeXML sol filename)
-    (VizGUI. false filename nil (make-enumerator) nil)))
+    (VizGUI. false filename nil (make-enumerator sigs) nil)))
 
 (defn run-fn [e & {:keys [ns sigs] :or {ns *ns*}}]
-  (show (exec/run-fn e :ns ns :sigs sigs)))
+  (show (exec/run-fn e :ns ns :sigs sigs) :ns ns :sigs sigs))
 
 (defmacro run [e & opts]
   `(run-fn (lang/expr ~e) ~@opts))
 
 (defn check-fn [e & {:keys [ns sigs] :or {ns *ns*}}]
-  (show (exec/check-fn e :ns ns :sigs sigs)))
+  (show (exec/check-fn e :ns ns :sigs sigs) :ns ns :sigs sigs))
 
 (defmacro check [e & opts]
   `(check-fn (lang/expr ~e) ~@opts))
